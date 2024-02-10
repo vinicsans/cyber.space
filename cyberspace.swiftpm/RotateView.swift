b91f67a5fb674af9be44a6b02b45374c910cf9ff
Sabrina <sabrinamonteiroslima@gmail.com>
2024-02-09 22:31:30 -0300

    feat: add home menu

---
+++ cyberspace.swiftpm/RotateView.swift
@@ -0,0 +1,18 @@
+//
+//  RotateView.swift
+//  cyber.space
+//
+//  Created by user on 09/02/24.
+//
+
+import SwiftUI
+
+struct RotateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotateView()
+}
---
+++ cyberspace.swiftpm/Scenes/Menu/HomePlanet.swift
@@ -0,0 +1,18 @@
+//
+//  HomePlanet.swift
+//  cyber.space
+//
+//  Created by user on 09/02/24.
+//
+
+import SwiftUI
+
+struct HomePlanet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePlanet()
+}
