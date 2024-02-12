867ae1c6e30d3ed7a8ff2138b52d007c6a3298af
Sabrina <sabrinamonteiroslima@gmail.com>
2024-02-12 15:45:45 -0300

    feat: planet intro scene

---
+++ cyberspace.swiftpm/Scenes/Menu/HomePlanetIntro.swift
@@ -0,0 +1,8 @@
+//
+//  HomePlanetIntro.swift
+//  cyber.space
+//
+//  Created by user on 12/02/24.
+//
+
+import Foundation
---
+++ cyberspace.swiftpm/Scenes/Menu/HomePlanetStart.swift
@@ -0,0 +1,85 @@
+//
+//  HomePlanet.swift
+//  cyber.space
+//
+//  Created by user on 09/02/24.
+//
+
+import SwiftUI
+
+struct HomePlanetView: View {
+    @EnvironmentObject var event: NextSceneEvent
+    
+    private let backgroundImage = Image("planet_background_x6")
+
+    var body: some View {
+        ZStack {
+            backgroundImage
+                .resizable()
+                .scaledToFill()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+
+            HStack(alignment: .center) {
+                Spacer()
+                UIPlanetView()
+            }
+            .padding(196)
+        }
+    }
+}
+
+// MARK: Interface
+
+struct UIPlanetView: View {
+    @EnvironmentObject private var event: NextSceneEvent
+    @State private var opacityAmount: Double = 0
+
+    var body: some View {
+        VStack(spacing: 16) {
+            Text("cyber.space")
+                .font(.system(size: 64, weight: .bold))
+                .foregroundColor(.black.opacity(0.75))
+
+            Button(action: { finishAnimation() }, label: {
+                Label("Start", systemImage: "play.fill")
+                    .font(.title2)
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 24)
+                    .background(.thinMaterial)
+                    .foregroundColor(.black.opacity(0.75))
+                    .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+            })
+        }
+        .opacity(opacityAmount)
+        .animation(.easeInOut(duration: 1), value: opacityAmount)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(3)) {
+                startAnimation()
+            }
+        }
+    }
+}
+
+
+// Animation
+
+extension UIPlanetView {
+    func startAnimation() {
+        opacityAmount = 100
+    }
+    
+    func finishAnimation() {
+        opacityAmount = 0
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(67000)) {
+            event.toggleScene()
+        }
+    }
+}
+
+
+#Preview {
+    HomePlanetView()
+}
+
